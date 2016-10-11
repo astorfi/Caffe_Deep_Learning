134d8ce847206e248026f33d3442015f1f11761b
Amirsina Torfi <astorfi@users.noreply.github.com>
2016-10-11 16:38:00 -0400

    Update OpenCV.sh

--- Installation/OpenCV_Installation/OpenCV.sh
+++ Installation/OpenCV_Installation/OpenCV.sh
@@ -14,7 +14,7 @@ cd opencv-$version
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
-make -j2
+make -j16
 sudo checkinstall
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
